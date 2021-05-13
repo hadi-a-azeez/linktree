6c45804c7288d72b98ce2e10fa5747962671bd41
hadi-a-azeez <hadikoduvally@gmail.com>
2021-05-13 18:05:08 +0530

    css not working

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import style from "./header.module.scss";
+import style from "./header.module.css";
 
 const Header: React.FC = () => {
   return (
-    <div className={style.container}>
-      <h1 style={{ marginLeft: "2rem" }} className={style.link}>
-        linktree
-      </h1>
-      <h1 className={style.link}>Blog</h1>
-      <h1 className={style.link}>Pricing</h1>
-      <h1 className={style.link}>Help</h1>
-    </div>
+    <>
+      <div className={style.container}>
+        <div className={style.links_wraper}>
+          <h1 className={style.link}>linktree</h1>
+          <h1 className={style.link}>Blog</h1>
+          <h1 className={style.link}>Pricing</h1>
+          <h1 className={style.link}>Help</h1>
+        </div>
+      </div>
+      <h1 style={{ marginLeft: "100px" }}>Hello world</h1>
+    </>
   );
 };
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,12 @@ import style from "./home.module.scss";
 import Header from "../components/header";
 
 const Home: React.FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <h1 style={{ marginLeft: "100px" }}>Shafi</h1>
+    </>
+  );
 };
 
 export default Home;
