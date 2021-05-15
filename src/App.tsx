670eb3cc05151aa94b81693192481b95bd9d6cd8
hadi-a-azeez <hadikoduvally@gmail.com>
2021-05-15 18:16:02 +0530

    working on home page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/home";
-import "./App.css";
+import "./App.scss";
 //import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App: React.FC = () => {
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-//import style from "./home.module.scss";
+import style from "./home.module.scss";
 import Header from "../components/header";
 
 const Home: React.FC = () => {
-  return <Header />;
+  return (
+    <div className={style.container}>
+      <Header />
+      <h1 className={style.heading_big}>The Only Link You’ll Ever Need</h1>
+      <h1 className={style.heading_normal}>
+        Connect audiences to all of your content with just one link
+      </h1>
+      <button className={style.btn}>GET STARTED FOR FREE</button>
+      <div className={style.link_wraper}>
+        <h1 className={style.text}>Already on Linktree?</h1>
+        <h1 className={style.link}>Log in</h1>
+      </div>
+      <div className={style.phone}>
+        <div className={style.phone_background}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
