f6d669a21d8d12bd3a62f49cad470ec159eaa51e
hadi-a-azeez <hadikoduvally@gmail.com>
2021-05-15 16:58:45 +0530

    navigation done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import style from "./header.module.css";
+import style from "./header.module.scss";
 
 const Header: React.FC = () => {
-  return <></>;
+  return (
+    <div className={style.container}>
+      <div className={style.links_wraper}>
+        <h1 className={style.link}>linktree</h1>
+        <h1 className={style.link}>Blog</h1>
+        <h1 className={style.link}>Pricing</h1>
+        <h1 className={style.link}>Help</h1>
+      </div>
+      <div className={style.buttons_wraper}>
+        <h1 className={style.link}>Log in</h1>
+        <button className={style.btn}>SIGN UP FREE</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 //import style from "./home.module.scss";
-//import Header from "../components/header";
+import Header from "../components/header";
 
 const Home: React.FC = () => {
-  return (
-    <div style={{ width: "100vw", background: "skyblue" }}>
-      <div
-        style={{
-          background: "red",
-          height: "100px",
-          width: "100px",
-          marginLeft: "100px",
-        }}
-      ></div>
-    </div>
-  );
+  return <Header />;
 };
 
 export default Home;
