f03d9c12c783e29f3e693e6465e39e12fead135e
hadi-a-azeez <hadikoduvally@gmail.com>
2021-05-17 18:42:42 +0530

    Added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Home from "./pages/home";
 import "./App.scss";
-//import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Register from "./pages/register";
 
 const App: React.FC = () => {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
