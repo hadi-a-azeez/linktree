75ff21be5bfd240d3bfa0d55a6ccc3f98b1de3bb
hadi-a-azeez <hadikoduvally@gmail.com>
2021-08-01 18:14:37 -0700

    mobile touch added

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import Register from "./pages/register";
 import Admin from "./pages/admin";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 
 const App: React.FC = () => {
+  const isMobile = window.innerWidth < 600;
   return (
     <Router>
       <Switch>
-        <DndProvider backend={HTML5Backend}>
+        <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
           <Route exact path="/" component={Home} />
           <Route path="/register" component={Register} />
           <Route path="/admin" component={Admin} />
