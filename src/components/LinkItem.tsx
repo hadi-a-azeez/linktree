a7ee211a9261aad43e7fbcd254761edc80df2e44
hadi-a-azeez <hadikoduvally@gmail.com>
2021-08-01 18:03:14 -0700

    sorting functionality implemented

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -1,24 +1,92 @@
 import React from "react";
 import styles from "./linkItem.module.scss";
-import { useDrag } from "react-dnd";
+import { useDrag, useDrop, DropTargetMonitor } from "react-dnd";
+import { useRef } from "react";
+import { XYCoord } from "dnd-core";
 
 type AppProps = {
   text: string;
+  index: number;
+  moveCardHandler: any;
+  setItems: any;
 };
 
-const LinkItem = ({ text }: AppProps) => {
-  const [{ opacity }, dragRef] = useDrag(
+interface DragItem {
+  index: number;
+  id: string;
+  type: string;
+}
+
+const LinkItem = ({ text, index, moveCardHandler, setItems }: AppProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [, drop] = useDrop({
+    accept: "ITEM",
+    hover(item: DragItem, monitor: DropTargetMonitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+
+      // Get pixels to the top
+      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      // Time to actually perform the action
+      moveCardHandler(dragIndex, hoverIndex);
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+  const [{ isDragging }, dragRef] = useDrag(
     () => ({
       type: "ITEM",
-      item: { text },
+      item: { name: "anything" },
       collect: (monitor) => ({
-        opacity: monitor.isDragging() ? 1 : 1,
+        isDragging: monitor.isDragging(),
       }),
     }),
     []
   );
+
+  const opacity = isDragging ? 0 : 1;
+
+  dragRef(drop(ref));
   return (
-    <div className={styles.card} ref={dragRef} style={{ opacity }}>
+    <div className={styles.card} ref={ref} style={{ opacity }}>
       <h1>{text}</h1>
     </div>
   );
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,18 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import LinkItem from "../components/LinkItem";
 import NavBar from "../components/NavBar";
 import styles from "./admin.module.scss";
+import { useDrop } from "react-dnd";
+
+export interface Item {
+  id: number;
+  text: string;
+}
+
+export interface itemsState {
+  items: Item[];
+}
+
+type array = {
+  id: number;
+  text: string;
+}[];
 
 const Admin: React.FC = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      text: "1",
+    },
+    {
+      id: 2,
+      text: "2",
+    },
+    {
+      id: 3,
+      text: "3",
+    },
+    {
+      id: 4,
+      text: "4",
+    },
+  ]);
+
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: "ITEM",
+    drop: () => ({ name: "anything" }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  const moveCardHandler = (dragIndex: number, hoverIndex: number) => {
+    const dragItem = items[dragIndex];
+
+    if (dragItem) {
+      setItems((prevState: array) => {
+        const coppiedStateArray: array = [...prevState];
+
+        const prevItem: array = coppiedStateArray.splice(
+          hoverIndex,
+          1,
+          dragItem
+        );
+
+        coppiedStateArray.splice(dragIndex, 1, prevItem[0]);
+
+        return coppiedStateArray;
+      });
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.grid_left}>
         <NavBar />
         <button className={styles.btn_main}>Add New Link</button>
-        <div className={styles.items_container}>
-          <LinkItem text="hello1" />
-          <LinkItem text="hello2" />
-          <LinkItem text="hello3" />
+        <div className={styles.items_container} ref={drop}>
+          {items.map((card, i) => (
+            <LinkItem
+              key={card.id}
+              index={i}
+              text={card.text}
+              moveCardHandler={moveCardHandler}
+              setItems={setItems}
+            />
+          ))}
         </div>
       </div>
     </div>
